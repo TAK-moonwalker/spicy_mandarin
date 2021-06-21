ed717e7c5ca012cf26735835f62f63dbc310b3b3
TAK-moonwalker <monsoon.tropical.bird@gmail.com>
2021-06-22 03:00:05 +0800

    add public folda

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
+
+const publicDir = path.join(__dirname, "../public")
+
+app.use(express.static(publicDir));
 
 // routing
 // spicy-mandarin.com/ => root
