386b56edce2e26d18fbb5676dde72affe10282d4
TAK-moonwalker <monsoon.tropical.bird@gmail.com>
2021-06-21 21:31:23 +0800

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3030;
+
+// routing
+// spicy-mandarin.com/ => root
+
+app.get('/', (req, res)=>{
+res.send('Hello Express!');
+})
+
+
+app.listen(port, ()=>{
+    console.log(`server running on Port:${port}`);
+})
