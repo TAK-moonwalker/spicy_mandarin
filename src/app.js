f077fbc5428535777434dc876c4ef2ecf43c2192
TAK-moonwalker <monsoon.tropical.bird@gmail.com>
2021-06-26 02:52:11 +0800

    partial front-end

--- src/app.js
+++ src/app.js
@@ -2,28 +2,35 @@ const path = require('path');
 const mongoose = require('mongoose')
 const express = require('express');
 const { SIGTERM } = require('constants');
+const hbs = require('hbs');
+const { send } = require('process');
 const app = express();
 const port = process.env.PORT || 3030;
 
-app.set('view engine', 'hbs');
-app.get('/',(req, res)=>{
-res.render('index', {
-    title: "Hishori Abe no site",
-    description: "Welcome to fastest site in japan"
-})
-})
-
+// define path for express
 const publicDir = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialPath = path.join(__dirname, "../templates/partials")
 
+// set-uo handlebars engine and path to views
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
+
+// set-up static directory to serve
 app.use(express.static(publicDir));
+
+app.get('/',(req, res)=>{
+res.render('index')
+})
 
 // routing
 // spicy-mandarin.com/ => root
 
-app.get('/', (req, res)=>{
-res.send('Hello Express!');
-})
 
+app.get('*', (req, res)=>{
+    send.res('404 not found!')
+})
 
 app.listen(port, ()=>{
     console.log(`server running on Port:${port}`);
