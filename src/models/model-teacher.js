ca0d20f18b082fe4a5b7141124883f4291ee6e3e
TAK-moonwalker <monsoon.tropical.bird@gmail.com>
2021-07-17 02:04:39 +0800

    Teacher Schema updated

--- src/models/model-teacher.js
+++ src/models/model-teacher.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
+const VideoSchema = new mongoose.Schema({
+    videoId:{
+        type: String
+    },
+    videoUrl: {
+        type: String
+    }
+})
+ 
 const teacherSchema = new mongoose.Schema({
     name: {
         type : String,
@@ -13,19 +22,12 @@ const teacherSchema = new mongoose.Schema({
         type : String,
     },
    videos: {
-       videoId : {
-         type: String
-       },
-       videoUrl : {
-         type: String
-       }
-   },
-   thumbnail: {
+       type : VideoSchema
+    },
 
-   },
-   profilePhoto:{
+   thumbnail: String,
 
-   }
+   profilePhoto: String
 })
 
 const Teacher = mongoose.model('Teacher', teacherSchema);
