40e0c9ee7f61a14f78d96c5c9e86e5345f03ec5a
TAK-moonwalker <monsoon.tropical.bird@gmail.com>
2021-07-24 18:54:19 +0800

    modal popoup fixed

--- public/assets/js/fetch-videos.js
+++ public/assets/js/fetch-videos.js
@@ -8,7 +8,7 @@ function loadVideos() {
     const urlPro = 'https://tak-spicymandarin.herokuapp.com/api/videos/lesson-spc'
 
    //Fetch videos from API
-fetch(urlPro, {
+fetch(urlDev, {
         method: 'get',
           dataType: 'json',
     })
@@ -38,34 +38,76 @@ fetch(urlPro, {
                 //console.log(div);
 
                 //modals generator
-                videoModal += 
-                `<div class="modal fade" id="modal${i}">
-                <div class="modal-dialog modal-dialog-centered modal-lg">
-                    <div class="modal-content">
-                <div class="modal-header">
-                    <h6>${itemList[i].title}</h6>
-                </div>
-                <div class="modal-body">
-                    <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${itemList[i].resourceId.videoId}"
-                           allowfullscreen></iframe>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
+            //     videoModal += 
+            // `<div class="modal fade" id="modal${i}">
+            //     <div class="modal-dialog modal-dialog-centered modal-lg">
+            //         <div class="modal-content">
+            //            <div class="modal-header">
+            //                 <h6>${itemList[i].title}</h6>
+            //            </div>
+            //                <div class="modal-body">
+            //                      <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+            //                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${itemList[i].resourceId.videoId}" allowfullscreen></iframe>
+            //                      </div>
+            //                               <div class="modal-footer">
+            //                                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            //                               </div>
                 
-                </div>
-                </div>
-                </div>
-                </div>`
-    
+            //                </div>
+            //            </div>
+            //        </div>
+            //  </div>`
+   
+
+            //generate modal
+             const divModal = document.createElement('div');
+             divModal.classList.add("modal", "fade");
+             divModal.id = `modal${i}`;
+             const divDialog = document.createElement("div");
+             divDialog.classList.add("modal-dialog", "modal-dialog-centered", "modal-lg");
+             const divContent = document.createElement("div");
+             divContent.classList.add("modal-content");
+             const divHeader = document.createElement("div");
+             divHeader.className = "modal-header";
+             const title = document.createElement("h6");
+             const titleText = document.createTextNode(`${itemList[i].title}`);
+             title.appendChild(titleText);
+             divHeader.appendChild(title);
+             const divBody = document.createElement("div");
+             divBody.className = "modal-body";
+             const divIframe = document.createElement("div");
+             divIframe.classList.add("embed-responsive", "embed-responsive-16by9", "z-depth-1-half");
+             const iframes = document.createElement("iframe");
+             iframes.classList.add("embed-responsive-item");
+             iframes.setAttribute("src", `https://www.youtube.com/embed/${itemList[i].resourceId.videoId}`);
+             iframes.setAttribute("allowfullscreen", true);
+             divIframe.appendChild(iframes);
+             const divFooter = document.createElement("div");
+             divFooter.classList.add("modal-footer");
+             const button = document.createElement("button");
+             button.classList.add("btn", "btn-secondary");
+             button.setAttribute("data-dismiss", "modal");
+             button.setAttribute("type", "button");
+             const buttonValue = document.createTextNode("Close");
+             button.appendChild(buttonValue);
+             divFooter.appendChild(button);
+             divBody.appendChild(divIframe);
+             divBody.appendChild(divFooter);
+             divContent.appendChild(divHeader);
+             divContent.appendChild(divBody);
+             divDialog.appendChild(divContent);
+             divModal.appendChild(divDialog);
+
+             modalLocation.appendChild(divModal);
+
+    console.log(divModal);
             } //for-loop end
 
             // console.log(imgGallery);
             // console.log(videoModal);
                 
             // gallery.innerHTML+= imgGallery;
-            modalLocation.innerHTML += videoModal;
+            //modalLocation.innerHTML += videoModal;
     
                 
             })
